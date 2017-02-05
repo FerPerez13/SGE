b6b6e4c4ee5e6d1207f1f5fa1349ce09e9703122
Unknown <dam.fperez@institutoserpis.org>
2017-02-06 00:35:52 +0100

    Realizado el ejercicio de School para repasar para el examen

---
+++ ModulosERP/School/__terp__.py
@@ -0,0 +1,13 @@
+{
+        "name" : "Diagrama1",
+        "version" : "0.1",
+        "author" : "Tiny",
+        "website" : "http://openerp.com",
+        "category" : "Unknown",
+        "description": """  """,
+        "depends" : ['base'],
+        "init_xml" : [ ],
+        "demo_xml" : [ ],
+        "update_xml" : ['Diagrama1_view.xml'],
+        "installable": True
+}
